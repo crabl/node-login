c45047a594b2c676df0845700014b84da4024a16
Chris Rabl <rabl.christopher@gmail.com>
2013-05-28 22:43:49 -0400

    Attempting to add a 'gatekeeper' function... failing

--- app/server/router.js
+++ app/server/router.js
@@ -3,6 +3,20 @@ var AM = require('./modules/account-manager');
 var EM = require('./modules/email-dispatcher');
 var SS = require('./modules/shirtsize-list');
 
+var gatekeeper = function(req, res) {
+    if(req.cookies.user == undefined || req.cookies.pass == undefined) {
+	res.render('login', { title: 'Hello - Please Login To Your Account' });
+    } else {
+	AM.autoLogin(req.cookies.user, req.cookies.pass, function(o){
+            if (o != null){
+		req.session.user = o;
+            } else {
+		res.render('login', { title: 'Hello - Please Login To Your Account' });
+            }
+	});
+    }
+}
+
 module.exports = function(app) {
 
     // debugging sessions //
@@ -23,7 +37,7 @@ module.exports = function(app) {
 	// check if the user's credentials are saved in a cookie //
 	if (req.cookies.user == undefined || req.cookies.pass == undefined){
 	    res.render('login', { title: 'Hello - Please Login To Your Account' });
-	}	else{
+	} else {
 	    // attempt automatic login //
 	    AM.autoLogin(req.cookies.user, req.cookies.pass, function(o){
 		if (o != null){
@@ -54,43 +68,40 @@ module.exports = function(app) {
     });
     
     // logged-in user homepage //
-    
     app.get('/home', function(req, res) {
-	if (req.session.user == null){
-	    // if user is not logged-in redirect back to login page //
-	    res.redirect('/');
-	}   else{
-	    res.render('home', {
-		title : 'WCHFF 2013 - Group Administration',
-		countries : CT,
-		udata : req.session.user
-	    });
-	}
+	gatekeeper(req, res);
+
+	res.render('home', {
+	    title : 'WCHFF 2013 - Group Administration',
+	    countries : CT,
+	    udata : req.session.user
+	});
     });
     
     app.post('/home', function(req, res){
-	if (req.param('user') != undefined) {
-	    AM.updateAccount({
-		user: req.param('user'),
-		name: req.param('name'),
-		email: req.param('email'),
-		groupname: req.param('groupname'),
-		country: req.param('country'),
-		pass: req.param('pass')
-	    }, function(e, o){
-		if (e){
-		    res.send('error-updating-account', 400);
-		}	else{
-		    req.session.user = o;
-		    // update the user's login cookies if they exists //
-		    if (req.cookies.user != undefined && req.cookies.pass != undefined){
-			res.cookie('user', o.user, { maxAge: 900000 });
-			res.cookie('pass', o.pass, { maxAge: 900000 });	
-		    }
-		    res.send('ok', 200);
+	gatekeeper(req, res);
+
+	AM.updateAccount({
+	    user: req.param('user'),
+	    name: req.param('name'),
+	    email: req.param('email'),
+	    groupname: req.param('groupname'),
+	    country: req.param('country'),
+	    pass: req.param('pass')
+	}, function(e, o){
+	    if (e){
+		res.send('error-updating-account', 400);
+	    } else {
+		// update the user's login cookies if they exists //
+		if (req.cookies.user != undefined && req.cookies.pass != undefined){
+		    res.cookie('user', o.user, { maxAge: 900000 });
+		    res.cookie('pass', o.pass, { maxAge: 900000 });	
 		}
-	    });
-	}	else if (req.param('logout') == 'true'){
+		res.send('ok', 200);
+	    }
+	});
+	
+	if (req.param('logout') == 'true'){
 	    res.clearCookie('user');
 	    res.clearCookie('pass');
 	    req.session.destroy(function(e){ res.send('ok', 200); });
@@ -99,101 +110,64 @@ module.exports = function(app) {
  
     // dance information page //
     app.get('/dances', function(req, res) {
-	console.log("Getting Dances...");
-	console.log(req.session.user.dances);
-	if (req.session.user == null){
-	    // if user is not logged-in redirect back to login page //
-	    res.redirect('/');
-	}   else{
-	    res.render('dances', {
-		title : 'WCHFF 2013 - Dance Information',
-		udata : req.session.user,
-		ddata : req.session.user.dances
-	    });
-	}
+	gatekeeper(req, res);
+
+	res.render('dances', {
+	    title : 'WCHFF 2013 - Dance Information',
+	    udata : req.session.user,
+	    ddata : req.session.user.dances
+	});
     });
     
     app.post('/dances', function(req, res){
-	console.log("Posting for "+req.session.user.user);
-	if (req.session.user != undefined) {
-	    AM.updateDances({
-		user: req.session.user.user,
-		dances: {
-		    'dance_hungariantitle': [req.param('dance1-hungariantitle'), 
-					     req.param('dance2-hungariantitle'),
-					     req.param('dance3-hungariantitle')],
-		    'dance_englishtitle': [req.param('dance1-englishtitle'),
-					   req.param('dance2-englishtitle'),
-					   req.param('dance3-englishtitle')],
-		    'dance_length' : [req.param('dance1-length'),
-				      req.param('dance2-length'), 
-				      req.param('dance3-length')],
-		    'dance_choreographer': [req.param('dance1-choreographer'),
-					    req.param('dance2-choreographer'),
-					    req.param('dance3-choreographer')],
-		    'dance_instructor': [req.param('dance1-instructor'),
-					 req.param('dance2-instructor'),
-					 req.param('dance3-instructor')],
-		    'dance_region': [req.param('dance1-region'),
-                                     req.param('dance2-region'),
-				     req.param('dance3-region')],
-		    'dance_type': [req.param('dance1-type'),
-                                   req.param('dance2-type'),
-				   req.param('dance3-type')],
-		    'dance_village': [req.param('dance1-village'),
-                                      req.param('dance2-village'),
-				      req.param('dance3-village')],
-		    'dance_description': [req.param('dance1-description'),
-					  req.param('dance2-description'),
-					  req.param('dance3-description')]
-		}
-	    }, function(e, o){
-		if (e){
-		    res.send('error-updating-account', 400);
-		}	else{
-		    req.session.user = o;
-		    /* ^---- 
-		       Okay folks, we've come to that point in the program where it's time to ask ourselves:
-		       "Honestly, what the fuck is going on here?"
-		       
-		       Whenever I console.log this shit, it always gives me "1". Dear ExpressJS, I didn't
-		       need the answer to the question "How many JavaScript coders does it take to create
-		       a fucking horrible mess of a framework and call it fucking Express?". No. All I want
-		       to know is why my goddamn session variable isn't working properly. To the writer of
-		       the 'node-login' framework: my deepest sympathies. I heard that you were committed to
-		       an insane asylum shortly after you finished writing your framework. I don't blame you.
-		       If this fucking shitmess of silly string and JS-brogrammer spooge is all I had to work
-		       with on a daily basis, mark my fucking words, I'd be in the foam hockey helmet right
-		       next to you. 
+	gatekeeper(req, res);
 
-		       How could anyone have thought that this "language", and above all, this bananarammer of
-		       a goddamn "framework" was a good idea? There is no rational, thinking, coherent human
-		       being capable of inflicting such atrocities on the world. WHY DO I DO THIS TO MYSELF?!
-		       Why did I listen to the proclamations of the brogrammers, heralding the arrival of a
-		       "tool" that has made me weep tears of my own blood?! 
-
-		       I tried. I really did try to like this language. I went so far as to almost LOVE it.
-		       But alas, it was not meant to be. Node.js has managed to suck every last drop of life
-		       out of me. It has turned me into a hollow shell of a man who only exists to free
-		       himself of the horrors that were committed against him by this trainwreck of a framework.
-
-		       And so, since nobody else is going to say it, I'm going to say it: I dislike Node.js.
-		       I _PROFOUNDLY_ dislike Express.js. I _EVEN MORE PROFOUNDLY_ dislike Javascript. It is
-		       a nasty, awful, monstrous, disgusting, wretched, evil, moronic, malevolent, vile,
-		       disfigured, whorish, retarded, schizophrenic, cocksucking, ass-munching, dumpster-diving,
-		       rotting carcass of a language. To those more tenacious than I, mazal tov.  To the rest 
-		       (and the sane), run fast, and run fucking far. /rant
-		    */
-		    res.send('success', 200); // Yay, success!
-		}
-	    });
-	}	else if (req.param('logout') == 'true'){
+	AM.updateDances({
+	    user: req.session.user.user,
+	    dances: {
+		'dance_hungariantitle': [req.param('dance1-hungariantitle'), 
+					 req.param('dance2-hungariantitle'),
+					 req.param('dance3-hungariantitle')],
+		'dance_englishtitle': [req.param('dance1-englishtitle'),
+				       req.param('dance2-englishtitle'),
+				       req.param('dance3-englishtitle')],
+		'dance_length' : [req.param('dance1-length'),
+				  req.param('dance2-length'), 
+				  req.param('dance3-length')],
+		'dance_choreographer': [req.param('dance1-choreographer'),
+					req.param('dance2-choreographer'),
+					req.param('dance3-choreographer')],
+		'dance_instructor': [req.param('dance1-instructor'),
+				     req.param('dance2-instructor'),
+				     req.param('dance3-instructor')],
+		'dance_region': [req.param('dance1-region'),
+                                 req.param('dance2-region'),
+				 req.param('dance3-region')],
+		'dance_type': [req.param('dance1-type'),
+                               req.param('dance2-type'),
+			       req.param('dance3-type')],
+		'dance_village': [req.param('dance1-village'),
+                                  req.param('dance2-village'),
+				  req.param('dance3-village')],
+		'dance_description': [req.param('dance1-description'),
+				      req.param('dance2-description'),
+				      req.param('dance3-description')]
+	    }
+	}, function(e, o){
+	    if (e){
+		res.send('error-updating-account', 400);
+	    } else {
+		//req.session.user = o;
+		res.send('success', 200); // Yay, success!
+	    }
+	});
+	
+	if (req.param('logout') == 'true'){
 	    res.clearCookie('user');
 	    res.clearCookie('pass');
 	    req.session.destroy(function(e){ res.send('ok', 200); });
 	}
     });
- 
    
     // creating new accounts //
     
