0a2754b44d6206b8814df53abfa1abcb42073ec1
Chris Rabl <rabl.christopher@gmail.com>
2013-06-01 01:21:59 -0400

    participant page is up

--- app/public/js/views/participants.js
+++ app/public/js/views/participants.js
@@ -1,3 +1,11 @@
+var initDatePicker = function() {
+    var eles = $('input[name=dob]:visible');
+    for (var i=0; i<eles.length; i++) {
+	eles[i].id = "dp"+i.toString();
+	$(eles[i]).datepicker({yearRange:"-100:+0", changeMonth:true, changeYear:true});
+	$('.ui-datepicker').css("font-size", "85%");
+    }
+};
 
 $(document).ready(function(){
     var hc = new ParticipantsController();
@@ -23,12 +31,14 @@ $(document).ready(function(){
     });
     $('#name-tf').focus();
 
-    // customize the account settings form //
-    
+    // initialize the datepicker //
+    initDatePicker();
+
+    // customize the account settings form //    
     $('#account-form h1').text('Account Settings');
     $('#user-tf').attr('disabled', 'disabled');
     $('#account-form-btn1').html('Delete Account');
     $('#account-form-btn1').addClass('btn-danger');
     $('#account-form-btn2').html('Save Changes');
 
-})
+});
